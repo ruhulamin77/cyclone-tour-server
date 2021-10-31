4049a4028e02e2ad4141f92d42ce537ec51ca20d
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-10-31 13:37:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
+const { MongoClient } = require('mongodb');
+const app = express()
+
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qu1uq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("cycloneTour");
+        const toursCollection = database.collection("tours");
+        const usersCollection = database.collection("users");
+
+        // create a document to insert
+
+        // GET Single Tour
+        app.get('/tours/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const tour = await toursCollection.findOne(query)
+            res.json(tour)
+        })
+
+        // GET API
+        app.get('/tours', async (req, res) => {
+            const cursor = toursCollection.find({});
+            const tours = await cursor.toArray()
+            res.send(tours)
+        })
+
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray()
+            res.send(users)
+        })
+
+
+
+
+
+        // POST API
+        app.post('/tours', async (req, res) => {
+            const tour = req.body;
+            console.log('hitting api', tour);
+            const result = await toursCollection.insertOne(tour)
+            res.json(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('hitting api', user);
+            const result = await usersCollection.insertOne(user)
+            res.json(result)
+        })
+
+
+
+
+        // UPDATE API
+
+
+
+
+        // DELETE API
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await usersCollection.deleteOne(query)
+            console.log('deleting user with id', result);
+            res.json(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
